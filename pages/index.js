07318d148664e0277cb84512a97c6c0985bc6bbc
Luke P <luke@lpdev.io>
2020-11-03 01:21:37 +1100

    Added image and some fancier styling

--- pages/index.js
+++ pages/index.js
@@ -9,16 +9,42 @@ function Home() {
 
   return (
     <>
-      <div className="container">
-        <h1>NextJS Base</h1>
+      <div className="background">
+        <div className="container">
+          <h1>NextJS Base</h1>
 
-        <p>A base structure for NextJS projects.</p>
+          <p>A base structure for NextJS projects.</p>
+        </div>
       </div>
 
       <style jsx>{`
+        @import "styles/mixins.scss";
+
+        .background {
+          background: url("images/retro-porche.jpg");
+          background-repeat: no-repeat;
+          background-size: cover;
+          background-position: 0% 50%;
+          width: 100vw;
+          height: 100vh;
+        }
+
         .container {
+          @include flex(column);
+          background: rgba(0,0,0,0.72);
           width: 100%;
-          text-align: center;
+          height: 100%;
+
+          h1 {
+            margin-top: 0;
+            font-size: 80px;
+            text-shadow: 0px 0px 10px rgba(0,0,0,0.3);
+          }
+
+          p {
+            color: var(--light-text);
+            font-size: 24px;
+          }
         }
       `}</style>
     </>
